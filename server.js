c580c87d83f2b3645d39836c3ca654b2600ded2c
gvasko79 <gabor.vaskoszedlar@gmail.com>
2016-08-29 22:36:16 +0200

    initial empty page

---
+++ server.js
@@ -0,0 +1,10 @@
+var connect = require('connect'),
+    serveStatic = require('serve-static'),
+    port = 3434;
+
+var app = connect();
+
+app.use(serveStatic(__dirname + '/src/client'));
+app.listen(port);
+
+console.log('Server running at http://localhost:' + port);
