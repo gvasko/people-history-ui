a28b8bdb4cce01eb0125f91416fc2df0a27a6700
gvasko79 <gabor.vaskoszedlar@gmail.com>
2016-08-31 22:36:40 +0200

    Add routing (reload does not work)

--- src/client/app/app.module.js
+++ src/client/app/app.module.js
@@ -1,8 +1,51 @@
 (function() {
 	'use strict';
 
-	angular.module('app', [
-
+	var app = angular.module('app', [
+		'ngRoute'
 	]);
+
+	app.config(function($routeProvider, $locationProvider) {
+
+		$routeProvider.when('/home', {
+			templateUrl: 'app/main-page/home.html'
+		});
+
+		$routeProvider.when('/documents', {
+			templateUrl: 'app/document-browser/document-browser.html'
+		});
+
+		$routeProvider.otherwise({ redirectTo: '/home'});
+
+		// $locationProvider.html5Mode({
+		// 	enabled: true,
+		// 	requireBase: false
+		// });
+
+	});
+
+	app.run(['$rootScope', function($rootScope) {
+		console.log('Adding route loggers ...');
+
+		$rootScope.$on("$stateChangeError", console.log.bind(console));
+
+		$rootScope.$on('$routeChangeStart', function(event, next, current) {
+			var currentPath = angular.isUndefined(current) ? '#' : current.originalPath;
+			var nextPath = next.originalPath;
+			console.log('Starting to leave %s to go to %s', currentPath, nextPath);
+		});
+
+		$rootScope.$on('$reload', function(event) {
+			console.log('Reloading');
+		});
+
+		$rootScope.$on('$routeChangeError', function(event, current, previous) {
+			var prevPath = angular.isUndefined(previous) ? '#' : previous.originalPath;
+			var currentPath = angular.isUndefined(current) ? '#' : current.originalPath;
+			console.log('Route error: from %s to %s', prevPath, currentPath);
+		});
+
+		console.log('... done');
+	}]);
 	
 })();
