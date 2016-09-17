10bb2299ffd20204192a6a372deab683e018f45d
gvasko79 <gabor.vaskoszedlar@gmail.com>
2016-09-17 20:57:10 +0200

    DocumentTile as directive

---
+++ src/client/app/document-browser/document-tile.directive.js
@@ -0,0 +1,19 @@
+(function() {
+	'use strict';
+
+	angular
+		.module('phDocumentBrowser')
+		.directive('phDocumentTile',phDocumentTile);
+
+	function phDocumentTile() {
+		var directive = {
+			scope: {
+				'doc': '='
+			},
+			templateUrl: 'app/document-browser/document-tile.html',
+			restrict: 'E'
+		};
+		return directive;
+	}
+
+})();
